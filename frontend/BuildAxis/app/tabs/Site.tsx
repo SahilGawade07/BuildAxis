b706a464397dd6ea66ee29ed02d566414da8adf8
Siddhu-0007 <chemtesiddharth97@gmail.com>
2025-08-07 21:59:32 +0530

    Tab navigation and home page ui

---
+++ frontend/BuildAxis/app/tabs/Site.tsx
@@ -0,0 +1,19 @@
+import { Text, View, StyleSheet, TextInput, TouchableOpacity, Image, ImageBackground, SafeAreaView, ScrollView } from "react-native";
+import React, { useState } from 'react';
+
+
+const Site = () => {
+
+ return(
+  <Text>Sites</Text>
+  );
+}
+
+export default Site;
+const styles = StyleSheet.create({
+
+ 
+
+});
+
+
---
+++ frontend/BuildAxis/app/tabs/_layout.tsx
@@ -0,0 +1,49 @@
+import React from "react";
+import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
+import Ionicons from "@expo/vector-icons/Ionicons";
+
+import Home from "@/app/tabs/home";
+import profile from "@/app/tabs/profile"
+import Site from "@/app/tabs/Site"
+
+
+const Tab = createBottomTabNavigator();
+
+export default function TabNavigator() {
+  return (
+    <Tab.Navigator
+      screenOptions={({ route }) => ({
+        tabBarStyle: {
+          backgroundColor: "white",
+          borderTopWidth: 0,
+        },
+        tabBarIcon: ({ color, size }) => {
+          let iconName: string;
+
+          switch (route.name) {
+            case "Home":
+              iconName = "home-outline";
+              break;
+            case "discover":
+              iconName = "compass-outline";
+              break;
+            case "Profile":
+              iconName = "person-outline";
+              break;
+            default:
+              iconName = "ellipse-outline";
+          }
+
+          return <Ionicons name={iconName as any} size={size} color={color} />;
+        },
+        tabBarActiveTintColor: "#0247D3",
+        tabBarInactiveTintColor: "black",
+        headerShown: false,
+      })}
+    >
+      <Tab.Screen name="Home" component={Home} />
+      <Tab.Screen name="Sites" component={Site} />
+      <Tab.Screen name="Profile" component={profile} />
+    </Tab.Navigator>
+  );
+}
