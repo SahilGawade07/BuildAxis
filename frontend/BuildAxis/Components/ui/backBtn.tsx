ffd832f4fa92b4b74da02c3283b3979ef0078552
Sahil Gawade <162673669+SahilGawade07@users.noreply.github.com>
2025-08-18 14:01:34 +0530

    fiexed bugs related to theme

--- frontend/BuildAxis/Components/ui/backBtn.tsx
+++ frontend/BuildAxis/Components/ui/backBtn.tsx
@@ -2,35 +2,31 @@ import { Entypo } from "@expo/vector-icons";
 import { router } from "expo-router";
 import React from "react";
 import { View, TouchableOpacity, Text, StyleSheet } from "react-native";
+import { useTheme } from "../../context/ThemeContext"; // adjust path
 
-export default function Back_Text_Butt({path,text}:any) {
-    return (
+export default function Back_Text_Butt({ path, text }: any) {
+  const { theme } = useTheme();
 
-        <View style={styles.header}>
-            <TouchableOpacity onPress={()=>router.push(path)} >
-
-                <Entypo name="chevron-left" size={30} color="black" />
-            </TouchableOpacity >
-            <View style={{ width: 6 }} /> {/* Placeholder for spacing */}
-            <Text style={styles.headerText}>{text}</Text>
-
-        </View>
-    )
-
+  return (
+    <View style={styles.header}>
+      <TouchableOpacity onPress={() => router.push(path)}>
+        <Entypo name="chevron-left" size={30} color={theme.text} />
+      </TouchableOpacity>
+      <View style={{ width: 6 }} />
+      <Text style={[styles.headerText, { color: theme.text }]}>{text}</Text>
+    </View>
+  );
 }
 
-
 const styles = StyleSheet.create({
-    container: { flex: 1, backgroundColor: "#fff" },
-    header: {
-        flexDirection: "row",
-        alignItems: "center",
-        paddingTop: 10,
-        paddingHorizontal: 15,
-        alignContent: "center",
-
-
-    },
-    headerText: { color: "#000000ff", fontSize: 25, fontWeight: "bold", }
-
+  header: {
+    flexDirection: "row",
+    alignItems: "center",
+    paddingTop: 10,
+    paddingHorizontal: 15,
+  },
+  headerText: {
+    fontSize: 25,
+    fontWeight: "bold",
+  },
 });
