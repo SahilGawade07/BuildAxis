1d11bb221578e95e036e69e6b534d3465b872b8d
Sahil Gawade <162673669+SahilGawade07@users.noreply.github.com>
2025-08-20 00:43:09 +0530

    fixed issue for mismatch in sequence of tab navigator

--- frontend/BuildAxis/app/(tabs)/_layout.tsx
+++ frontend/BuildAxis/app/(tabs)/_layout.tsx
@@ -32,7 +32,7 @@ export default function TabLayout() {
       }}
     >
       <Tabs.Screen
-        name="home"
+        name="home/index"
         options={{
           title: "Home",
           tabBarIcon: ({ color, focused }) => (
